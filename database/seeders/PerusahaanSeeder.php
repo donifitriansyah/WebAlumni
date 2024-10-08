96381b15d3063cc21d35ea1f6a95c48e0082ce9d
Doni Fitriansyah <102551960+donifitriansyah@users.noreply.github.com>
2024-10-09 00:58:12 +0700

    kasi seeder biargamnteng

--- database/seeders/PerusahaanSeeder.php
+++ database/seeders/PerusahaanSeeder.php
@@ -17,10 +17,10 @@ class PerusahaanSeeder extends Seeder
     {
         DB::table('perusahaan')->insert([
             [
-                'id_user' => 1,
+                'id_user' => 5,
                 'nama_perusahaan' => 'PT. Mencari Cinta Sejati',
                 'nib' => '123456789',
-                'alamat' => 'Jalan Contoh No. 1, Jakarta',
+                'alamat' => 'Jalan Contoh No. 1, Pinyuh',
                 'email_perusahaan' => 'info@contohperusahaan.com',
                 'sektor_bisnis' => 'Teknologi',
                 'deskripsi_perusahaan' => 'Perusahaan yang bergerak di bidang teknologi informasi.',
@@ -32,10 +32,10 @@ class PerusahaanSeeder extends Seeder
                 'updated_at' => Carbon::now(),
             ],
             [
-                'id_user' => 2,
+                'id_user' => 6,
                 'nama_perusahaan' => 'CV. Terus Berusahahaha',
                 'nib' => '987654321',
-                'alamat' => 'Jalan Usaha No. 2, Bandung',
+                'alamat' => 'Jalan Usaha No. 2, Kakap',
                 'email_perusahaan' => 'info@contohusaha.com',
                 'sektor_bisnis' => 'Perdagangan',
                 'deskripsi_perusahaan' => 'Perusahaan perdagangan yang sudah berpengalaman.',
