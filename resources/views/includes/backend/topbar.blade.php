c43853b87f9715a1afcbb26c7abf71a106897092
Doni Fitriansyah <102551960+donifitriansyah@users.noreply.github.com>
2024-10-12 01:04:24 +0700

    update topbar

--- resources/views/includes/backend/topbar.blade.php
+++ resources/views/includes/backend/topbar.blade.php
@@ -1,13 +1,11 @@
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
     <!-- Sidebar Toggle (Topbar) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
 
     <!-- Topbar Search -->
-    <form
-        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                 aria-label="Search" aria-describedby="basic-addon2">
@@ -103,15 +101,23 @@
 
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->username }}</span>
-                <img class="img-profile rounded-circle"
-                    src="img/undraw_profile.svg">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                    @if (Auth::user()->role === 'admin')
+                        {{ Auth::user()->admin->nama }}
+                    @elseif(Auth::user()->role === 'alumni')
+                        {{ Auth::user()->alumni->nama_alumni }}
+                    @elseif(Auth::user()->role === 'perusahaan')
+                        {{ Auth::user()->perusahaan->nama_perusahaan }}
+                    @else
+                        {{ Auth::user()->username }}
+                    @endif
+                </span>
+                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
             </a>
             <!-- Dropdown - User Information -->
-            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="userDropdown">
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
