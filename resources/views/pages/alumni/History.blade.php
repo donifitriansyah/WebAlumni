e321f863340c8778ee69856c859e692b98e8b8ec
Your Name <you@example.com>
2024-11-04 22:38:45 +0700

    update profile alumni

--- resources/views/pages/alumni/History.blade.php
+++ resources/views/pages/alumni/History.blade.php
@@ -1,16 +1,64 @@
 @extends('layouts.alumni')
+
 @section('title')
 History Alumni
 @endsection
+
 @section('content-alumni')
-    <div class="container-fluid">
+<div class="container-fluid">
 
-        <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">History Alumni</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-        </div>
-
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">History Alumni</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+            <i class="fas fa-download fa-sm text-white-50"></i> Generate Report
+        </a>
     </div>
+
+    <!-- Application History Table -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Application History</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>ID Lamaran</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>CV</th>
+                            <th>Transkrip Nilai</th>
+                            <th>Portofolio</th>
+                            <th>Status</th>
+                            <th>Tanggal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (isset($applications) && !empty($applications))
+                            @foreach($applications as $application)
+                                <tr>
+                                    <td>{{ $application['id_lamaran'] }}</td>
+                                    <td>{{ $application['nama'] }}</td>
+                                    <td>{{ $application['email'] }}</td>
+                                    <td><a href="{{ asset('path/to/cv/'.$application['cv']) }}">Download</a></td>
+                                    <td><a href="{{ asset('path/to/transkrip/'.$application['transkrip_nilai']) }}">Download</a></td>
+                                    <td><a href="{{ asset('path/to/portopolio/'.$application['portopolio']) }}">View</a></td>
+                                    <td>{{ $application['status'] }}</td>
+                                    <td>{{ \Carbon\Carbon::parse($application['created_at'])->format('d-m-Y') }}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="8" class="text-center">No application history found.</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
 @endsection
--- resources/views/pages/alumni/dashboard_alumni.blade.php
+++ resources/views/pages/alumni/dashboard_alumni.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.alumni')
-@section('title', 'Dashboard')
-
+@section('title')
+Dashboard
+@endsection
 @section('content-alumni')
     <div class="container-fluid">
+
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard Alumni</h1>
@@ -14,10 +16,10 @@
         <!-- Content Row -->
         <div class="row mb-4">
             @foreach ([
-                ['title' => 'Earnings (Monthly)', 'value' => '$40,000', 'icon' => 'calendar', 'color' => 'primary'],
-                ['title' => 'Earnings (Annual)', 'value' => '$215,000', 'icon' => 'dollar-sign', 'color' => 'success'],
-                ['title' => 'Tasks Completed', 'value' => '50%', 'icon' => 'clipboard-list', 'color' => 'info', 'progress' => true],
-                ['title' => 'Pending Requests', 'value' => '5', 'icon' => 'comments', 'color' => 'warning']
+                ['title' => 'Lamaran di Input', 'value' => '9', 'icon' => 'clipboard-list', 'color' => 'primary'],
+                ['title' => 'Lamaran Ditolak', 'value' => '3', 'icon' => 'times-circle', 'color' => 'success'],
+                ['title' => 'Lamaran Diterima', 'value' => '4', 'icon' => 'check-circle', 'color' => 'info', 'progress' ],
+                ['title' => 'Lamaran Menunggu', 'value' => '2', 'icon' => 'comments', 'color' => 'warning']
             ] as $card)
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-{{ $card['color'] }} shadow h-100 py-2">
@@ -50,75 +52,7 @@
             @endforeach
         </div>
 
-        <!-- Charts Row -->
-        <div class="row mb-4">
-            <div class="col-xl-8 col-lg-7">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3 d-flex align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart-area">
-                            <canvas id="myAreaChart"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-5">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3 d-flex align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart-pie pt-4 pb-2">
-                            <canvas id="myPieChart"></canvas>
-                        </div>
-                        <div class="mt-4 text-center small">
-                            <span class="mr-2"><i class="fas fa-circle text-primary"></i> Direct</span>
-                            <span class="mr-2"><i class="fas fa-circle text-success"></i> Social</span>
-                            <span class="mr-2"><i class="fas fa-circle text-info"></i> Referral</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- No Charts Row or Projects Row -->
 
-        <!-- Projects Row -->
-        <div class="row">
-            <div class="col-lg-6 mb-4">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Current Projects</h6>
-                    </div>
-                    <div class="card-body">
-                        @foreach ([
-                            ['title' => 'Server Migration', 'progress' => '20%', 'bg' => 'danger'],
-                            ['title' => 'Sales Tracking', 'progress' => '40%', 'bg' => 'warning'],
-                            ['title' => 'Customer Database', 'progress' => '60%', 'bg' => 'info'],
-                            ['title' => 'Payout Details', 'progress' => '80%', 'bg' => 'info'],
-                            ['title' => 'Account Setup', 'progress' => 'Complete!', 'bg' => 'success']
-                        ] as $project)
-                            <h4 class="small font-weight-bold">{{ $project['title'] }} <span class="float-right">{{ $project['progress'] }}</span></h4>
-                            <div class="progress mb-4">
-                                <div class="progress-bar bg-{{ $project['bg'] }}" role="progressbar" style="width: {{ $project['progress'] == 'Complete!' ? '100%' : $project['progress'] }}" aria-valuenow="{{ rtrim($project['progress'], '%') }}" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-6 mb-4">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                    </div>
-                    <div class="card-body text-center">
-                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="Illustration">
-                        <p>Add some quality, SVG illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful SVG images that you can use completely free and without attribution!</p>
-                        <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
