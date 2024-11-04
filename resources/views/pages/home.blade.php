14be2b8cfdae5023ac6b093cb8849617bf11a17a
Doni Fitriansyah <102551960+donifitriansyah@users.noreply.github.com>
2024-11-05 05:18:42 +0700

    final

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,28 +7,27 @@
 <!-- Tambahkan Modal -->
 <div class="modal fade" id="welcomeModal" tabindex="-1" aria-labelledby="welcomeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
+        <div class="modal-content shadow-lg border-0 rounded">
+            <div class="modal-header bg-primary text-white">
                 <h5 class="modal-title" id="welcomeModalLabel">Tracer Study 2024</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <p>Tracer Study 2024 adalah kegiatan Survey Penelusuran Alumni yang digunakan untuk melihat keterserapan Alumni di Industri, Wirausaha dan Studi Lanjut. Data ini sangat diperlukan untuk kegiatan Akreditasi Prodi.</p>
+            <div class="modal-body text-justify">
+                <p class="lead" style="font-family: Poppins; font-size: 20px">Tracer Study 2024 adalah kegiatan Survey Penelusuran Alumni yang digunakan untuk melihat keterserapan Alumni di Industri, Wirausaha dan Studi Lanjut. Data ini sangat diperlukan untuk kegiatan Akreditasi Prodi.
 
-                <p>Kami sangat menghargai kesediaan Alumni dalam meluangkan waktunya untuk kegiatan ini.</p>
-
-                <p>Semoga kegiatan Tracer Study ini dapat menjadi amal jariyah bagi kita semua.</p>
-
-                <p>Atas Perhatian dan Kerjasamanya, kami mengucapkan terima kasih yang sebesar-besarnya.</p>
+                   <br> Kami sangat menghargai kesediaan Alumni dalam meluangkan waktunya untuk kegiatan ini <br>Semoga kegiatan Tracer Study ini dapat menjadi amal jariyah bagi kita semua. <br> Atas Perhatian dan Kerjasamanya, kami mengucapkan terima kasih yang sebesar-besarnya.
+                </p>
 
                 <!-- Tambahkan link Google Form jika diperlukan -->
-                <a href="#" class="text-primary" data-bs-dismiss="modal">Kalau Merasa Sudah KLIK Disini Yaa</a>
+                <a href="#" class="text-primary fw-bold" data-bs-dismiss="modal">Kalau Merasa Sudah KLIK Disini Yaa</a>
             </div>
             <div class="modal-footer">
-                <a href="/register-alumni" class="btn btn-primary">Yuk isi Tracer Study</a>
+                <a href="/register-alumni" class="btn btn-success">Yuk isi Tracer Study</a>
             </div>
         </div>
     </div>
 </div>
+
 
 @section('content')
     <!-- BERITA -->
